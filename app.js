2e88e69f2909f428b1f5c8f138f453fa142d3879
sunil pal <sunil5568.pal@gmail.com>
2026-04-12 15:59:49 +0530

    suni name added

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end("Hello from Kubernetes 🚀");
+  res.end("Hello from Kubernetes. This is sunil pal");
 });
 
 server.listen(3000, () => {
