8f86de73f4ee5811bfb03efe7caa1ec6d6f55475
sunil pal <sunil5568.pal@gmail.com>
2026-04-18 19:00:10 +0530

    deploy to aws

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end("Hello from Kubernetes. This is sunil pal. this is the third commit");
+  res.end("Hello from Kubernetes. This is sunil pal. this is the third commit. This is just to confirm that this is deployed on aws instances");
 });
 
 server.listen(3000, () => {
